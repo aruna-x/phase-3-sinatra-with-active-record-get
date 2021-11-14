38f881b460852c2ce1891e2de7f6e7c76715148d
Aruna <aruna.evan15@gmail.com>
2021-11-14 14:32:54 -0800

    commit tree 6f77d8e5af4e3031149b949e7a48ecba8ca30b17

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
   get '/' do
     { message: "Hello world" }.to_json
+  end
+
+  get '/games' do
+    Game.all.order(:title).limit(10).to_json
+  end
+
+  get '/games/:id' do
+    Game.find(params[:id]).to_json(only: [:id, :title, :genre, :price], include: {reviews: {only:[:comment, :score], include: {user: {only: [:name]}}}})
   end
 
 end
